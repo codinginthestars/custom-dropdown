168d23aba37aec6046e6c8a8087896918ca954d5
William <codinginthestars@gmail.com>
2024-09-24 10:11:01 -0500

    added functionality to dropdown buttons

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import './styles.css';
+
+const dropButtonOne = document.getElementById("drop-button");
+const dropButtonTwo = document.getElementById("drop-button-two");
+
+dropButtonOne.addEventListener("click", () => {
+   dropdownOne();
+});
+
+dropButtonTwo.addEventListener("click", () => {
+   dropdownTwo();
+})
+
+function dropdownOne() {
+   const optionOne = document.getElementById("optionOne");
+
+   optionOne.classList.toggle("show");
+};
+
+function dropdownTwo() {
+   const optionTwo = document.getElementById("optionTwo");
+
+   optionTwo.classList.toggle("show");
+}
